548bf20b64dd345c6580cacdc8278799a0a3ca25
Grazi Berti <limaberti.grazi@gmail.com>
2025-02-16 09:12:04 -0300

    create app reminder

---
+++ Reminder/ReminderTests/ReminderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderTests.swift
+//  ReminderTests
+//
+//  Created by Grazi  Berti on 16/02/25.
+//
+
+import Testing
+@testable import Reminder
+
+struct ReminderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
